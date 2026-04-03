4c1e067b2089340c1264fbb01b1d15e1784fa771
Sooriyansh <sooriyashpatel@gmail.com>
2026-04-03 15:06:19 +0530

    Delete src directory

--- src/components/OrnamentDivider.jsx
+++
@@ -1,9 +0,0 @@
-export default function OrnamentDivider() {
-  return (
-    <div className="ornament-divider">
-      <div className="ornament-line" />
-      <div className="ornament-diamond" />
-      <div className="ornament-line r" />
-    </div>
-  );
-}
